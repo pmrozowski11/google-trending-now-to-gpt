8b19ea073859256e69b97ae37f7bd3a995132089
pmrozowski11 <pmrozowski11@gmail.com>
2025-05-07 14:18:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ app.add_middleware(CORSMiddleware, allow_origins=["*"])
 def get_trends():
     try:
         pytrends = TrendReq(hl='pl-PL', tz=360)
-        # trending_searches to lista ogólnych trendów, nie tylko z kategorii "Rozrywka"
-        trending_searches_df = pytrends.trending_searches(pn='poland')
-        trending = trending_searches_df[0].tolist()
-        return {"trendy": trending}
+        kw_list = ["film", "serial", "muzyka"]  # frazy startowe, by zbudować kontekst
+        pytrends.build_payload(kw_list, cat=6, geo='PL', timeframe='now 1-d')
+        related = pytrends.related_queries()
+        trendy = []
+        for keyword, queries in related.items():
+            if queries and 'top' in queries and queries['top'] is not None:
+                trendy.extend(queries['top']['query'].tolist())
+        unique_trendy = list(set(trendy))
+        return {"trendy": unique_trendy[:10]}  # zwracamy tylko top 10
     except Exception as e:
         return {"error": str(e)}
